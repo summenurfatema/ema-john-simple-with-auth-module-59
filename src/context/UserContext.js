0425ba8083f9d7d921f9232f8e03b9c9fd99cef0
Fatema04 <shadrinmoni15@gmail.com>
2022-10-19 22:11:24 -0700

    user contextrunning

--- src/context/UserContext.js
+++ src/context/UserContext.js
@@ -1,8 +1,11 @@
 import React, { createContext } from 'react';
-const AuthContext = createContext()
 
-export const UserContext = ({ children }) => {
-    const authInfo = {}
+
+export const AuthContext = createContext()
+
+const UserContext = ({ children }) => {
+    const user = { email: 'web@email.com' }
+    const authInfo = { user }
     return (
         <AuthContext.Provider value={authInfo}>
             {children}
