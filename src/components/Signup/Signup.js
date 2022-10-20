74862634532b7d14ccbe6d9f6ed5135a8290afc1
Fatema04 <shadrinmoni15@gmail.com>
2022-10-19 21:52:38 -0700

    sign up

--- src/components/Signup/Signup.js
+++ src/components/Signup/Signup.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Signup = () => {
     return (
-        <div>
-            this is signup
+        <div className='form-container'>
+            <h2 className='form-title'>Signup</h2>
+            <form>
+                <div className='form-control'>
+                    <label htmlFor='email'>Email</label>
+                    <input type='email' name="email" />
+                </div>
+                <div className='form-control'>
+                    <label htmlFor='password'>Password</label>
+                    <input type='password' name="password" />
+                </div>
+                <div className='form-control'>
+                    <label htmlFor='confirm'>Confirm Password</label>
+                    <input type='password' name="confirm" />
+                </div>
+                <input className='btn-submit' type='submit' value='Sign Up' />
+                <p>Already have an account ? <Link to='/login'>Login here</Link> </p>
+
+            </form>
+
         </div>
     );
 };
