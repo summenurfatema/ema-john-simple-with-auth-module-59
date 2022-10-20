0241e20631ca8daec28a09feaff3b614dce4921e
Fatema04 <shadrinmoni15@gmail.com>
2022-10-19 22:17:33 -0700

    user context running

--- src/components/Signup/Signup.js
+++ src/components/Signup/Signup.js
@@ -2,10 +2,20 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Signup = () => {
+
+    const handleSubmit = event => {
+        event.preventDefault()
+        const form = event.target
+        const email = form.email.value
+        const password = form.password.value
+        const confirm = form.confirm.value
+        console.log(email, password, confirm)
+    }
+
     return (
         <div className='form-container'>
             <h2 className='form-title'>Signup</h2>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <div className='form-control'>
                     <label htmlFor='email'>Email</label>
                     <input type='email' name="email" />
