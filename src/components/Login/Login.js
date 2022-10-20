f0bb882cbd98106d4c1d3cb0e7bfaa775e0a8b82
Fatema04 <shadrinmoni15@gmail.com>
2022-10-19 21:24:01 -0700

    login

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,9 +1,21 @@
 import React from 'react';
-
+import './Login.css'
 const Login = () => {
     return (
-        <div>
-            this is login
+        <div className='form-container'>
+            <h2 className='form-title'>Login</h2>
+            <form>
+                <div className='form-control'>
+                    <label htmlFor='email'>Email</label>
+                    <input type='email' name="email" />
+                </div>
+                <div className='form-control'>
+                    <label htmlFor='password'>Password</label>
+                    <input type='password' name="password" />
+                </div>
+
+            </form>
+
         </div>
     );
 };
