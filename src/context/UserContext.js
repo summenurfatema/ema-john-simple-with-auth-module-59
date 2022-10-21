504850b7f6e00e11e3e0e62b58fe2c61b08b1533
Fatema04 <shadrinmoni15@gmail.com>
2022-10-20 18:54:40 -0700

    login added

--- src/context/UserContext.js
+++ src/context/UserContext.js
@@ -1,5 +1,5 @@
 import React, { createContext, useState } from 'react';
-import { createUserWithEmailAndPassword, getAuth } from 'firebase/auth'
+import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword } from 'firebase/auth'
 import app from '../firebase/firebase.config'
 
 
@@ -9,14 +9,18 @@ const auth = getAuth(app)
 const UserContext = ({ children }) => {
     const [user, setUser] = useState(null)
 
-    // signup
-
+    //----------------signup--------------//
     const createUser = (email, password) => {
         return createUserWithEmailAndPassword(auth, email, password)
     }
 
+    //------------------sign in--------------//
+    const signIn = (email, password) => {
+        return signInWithEmailAndPassword(auth, email, password)
+    }
 
-    const authInfo = { user, createUser }
+
+    const authInfo = { user, createUser, signIn }
     return (
         <AuthContext.Provider value={authInfo}>
             {children}
