cd5feab30c52538b0a6ae22de944753925d00aab
Fatema04 <shadrinmoni15@gmail.com>
2022-10-20 19:14:37 -0700

    unsubscribe added

--- src/context/UserContext.js
+++ src/context/UserContext.js
@@ -1,5 +1,5 @@
-import React, { createContext, useState } from 'react';
-import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword, signOut } from 'firebase/auth'
+import React, { createContext, useEffect, useState } from 'react';
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut } from 'firebase/auth'
 import app from '../firebase/firebase.config'
 
 
@@ -23,6 +23,15 @@ const UserContext = ({ children }) => {
     const logOut = () => {
         return signOut(auth)
     }
+    // -----------------subscribe--------------//
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, currentUser => {
+            console.log(currentUser)
+            setUser(currentUser)
+        })
+        return () => unsubscribe()
+    }, [])
     const authInfo = { user, createUser, signIn, logOut }
     return (
         <AuthContext.Provider value={authInfo}>
