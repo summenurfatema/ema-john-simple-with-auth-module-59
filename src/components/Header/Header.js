00cf48d71e06edf829160764e81f3144e1c3c81a
Fatema04 <shadrinmoni15@gmail.com>
2022-10-20 19:24:55 -0700

    unsubscribe added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,13 @@
 
+import { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../../context/UserContext';
 
 import logo from '../../images/Logo.svg';
 import './Header.css';
 
 const Header = () => {
-
+    const { user } = useContext(AuthContext)
     return (
         <nav className='header'>
             <img src={logo} alt="" />
@@ -16,6 +18,7 @@ const Header = () => {
                 <Link to="/about">About</Link>
                 <Link to="/login">Log in</Link>
                 <Link to="/signup">Sign up</Link>
+                <span className='span'>{user?.email}</span>
 
             </div>
         </nav>
